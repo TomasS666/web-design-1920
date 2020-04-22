fcad0995df5d2e0b641fba1d201f371240906798
TomasS <43637908+GrimReaper6667@users.noreply.github.com>
2020-04-22 12:52:05 +0200

    latest update

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,10 +1,50 @@
-window.addEventListener("keydown", (e)=>{
-    console.log(e.keyCode)
-    if(e.keyCode == 101){
-        console.log("go back")
-        window.history.back()
+// window.addEventListener("keydown", (e)=>{
+//     console.log(e.keyCode)
+//     if(e.keyCode == 101){
+//         console.log("go back")
+//         window.history.back()
+//     }
+// })
+
+function isPage(term) {
+    return window.location.href.indexOf(term) > -1
+}
+
+
+
+window.onload = function(){
+    
+    if(isPage('/index.html')){
+        console.log("yes")
+        greeting()
     }
-})
+    function greeting(date = new Date()){
+        console.log(' yes')
+       const hour =  date.getHours()
+
+        const greeting = document.querySelector('span[data-daytime-greeting]')
+
+        if(hour <= 11){      
+            greeting.textContent = "Goedenmorgen"
+        }else if(hour <= 17){
+            greeting.textContent = "Goedenmiddag"
+        }else if(hour <= 23){
+            greeting.textContent = "Goedenavond"
+        }else if(hour <= 3){
+            greeting.textContent = "Goedenacht"
+        }else{
+            greeting.textContent = "Goedendag"
+        }
+    }
+    
+   
+
+   
+
+}
 
 
-console.log("yes")
+
+
+
+// console.log("yes")
